bb2d2b7bc76c438b8e2e735c3424cca166c56a22
Rubem Almeida <1538506+rubemalmeida@users.noreply.github.com>
2022-08-02 09:30:25 -0300

    reverse notification logic

--- background.js
+++ background.js
@@ -8,16 +8,16 @@ chrome.commands.onCommand.addListener((command) => {
     },
     (isMute) => {
       if(isMute[0].result === false) {
-        chrome.action.setIcon({path: '/assets/icon-no-speak-16x.png'});
-        chrome.action.setTitle({title: 'Muted microphone'});
-        chrome.action.setBadgeText({text: 'OFF'})
-        chrome.action.setBadgeBackgroundColor({color: 'blue'});
-      }
-      else {
         chrome.action.setIcon({path: '/assets/icon-normal-16x.png'});
         chrome.action.setTitle({title: 'All can ear you'});
         chrome.action.setBadgeText({text: 'ON'})
         chrome.action.setBadgeBackgroundColor({color: 'red'});
+      }
+      else {
+        chrome.action.setIcon({path: '/assets/icon-no-speak-16x.png'});
+        chrome.action.setTitle({title: 'Muted microphone'});
+        chrome.action.setBadgeText({text: 'OFF'})
+        chrome.action.setBadgeBackgroundColor({color: 'blue'});
       }
       setTimeout(() => {
         chrome.action.setBadgeText({text: ''})
